02c39f0d51b44a7ce763268f51f760b6e7ce969e
Jörg Bunse <joerg@w541.bunse.local>
2024-08-08 16:09:01 +0200

    First attempt to parse the json response

---
+++ farm-info.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import requests
+from urllib.request import urlopen
+import json
+
+res = requests.get('http://192.168.2.154/farm-info')
+
+print(res.text)
+j_obj = json.loads(res.text)
+print('Wasserstand ',j_obj['productTypeString'])
+print('Reihe 1: ',j_obj['rows'][0]['waterLevel'],'%')
+print('Reihe 2: ',j_obj['rows'][1]['waterLevel'],'%')
+print('Reihe 3: ',j_obj['rows'][2]['waterLevel'],'%')
