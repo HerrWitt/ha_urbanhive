3cbfc4b02db894d8c85bddf2b57033594a31ab92
HerrWitt <joerg.bunse@outlook.com>
2024-08-09 10:10:42 +0200

    Cleaned up

--- farm-info.py
+++ farm-info.py
@@ -1,12 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.12
+
+#
+# Import libraries
+#
 import requests
-from urllib.request import urlopen
 import json
 
+#
+# Get & load data from homefarm
+#
 res = requests.get('http://192.168.2.154/farm-info')
-
-print(res.text)
 j_obj = json.loads(res.text)
+
+#
+# Display information
+#
 print('Wasserstand ',j_obj['productTypeString'])
 print('Reihe 1: ',j_obj['rows'][0]['waterLevel'],'%')
 print('Reihe 2: ',j_obj['rows'][1]['waterLevel'],'%')
